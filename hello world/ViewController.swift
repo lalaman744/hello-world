51fd49235876129b112592594a460a4eddc31a0c
Josh Kleinschmidt <tf8546hu@t3040-2-6.local>
2019-01-15 18:30:00 -0600

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Josh Kleinschmidt on 1/15/19.
+//  Copyright © 2019 Josh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
